60e31e1c6e6645b997383d2aafbf9d119b522204
Cristian Spinetta <cristian.spinetta@despegar.com>
2016-02-13 16:18:41 -0300

    wip: minor changes on InstrumentationLoaderSpec

--- agent/src/test/scala/kamon/agent/InstrumentationLoaderSpec.scala
+++ agent/src/test/scala/kamon/agent/InstrumentationLoaderSpec.scala
@@ -1,12 +1,25 @@
 package kamon.agent
 
+import java.lang.instrument.Instrumentation
+
+import org.mockito.Mockito
 import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
-import utils.FakeInstrumentation
 
 class InstrumentationLoaderSpec extends WordSpecLike with Matchers with BeforeAndAfterAll {
 
   //  "should redefine API KamonInstrumentation by the agent" in {
-  //    InstrumentationLoader.loadInstrumentationAPI(FakeInstrumentation)
-  //  }
+
+  "should run InstrumentationLoader correctly with empty config < kamon.agent.instrumentations >" in {
+    val instrumentationMock = Mockito.mock(classOf[Instrumentation])
+    InstrumentationLoader.load(instrumentationMock)
+  }
+
+  // TODO: necesitamos pasarle al InstrumentationLoader.load la config por parametro, asi testeamos con distinta config
+  // Ademas no tiene que romper por que no encuentre la configuración, o por no poder parsearla bien,
+  // con lo cual podemos tener un objeto ConfigAgent que parsee la configuracion
+  "should not break by an unknown Instrumentation" in {
+    //    val instrumentationMock = Mockito.mock(classOf[Instrumentation])
+    //    InstrumentationLoader.load(instrumentationMock)
+  }
 
 }
