e1822990a6a675a0646a86985a99f556a0ab43b8
Cristian Spinetta <cebs91@gmail.com>
2016-02-19 21:12:56 -0300

    wip: Add @Value to Interception Builder

--- agent/src/main/java/kamon/agent/api/instrumentation/interceptor/Interception.java
+++ agent/src/main/java/kamon/agent/api/instrumentation/interceptor/Interception.java
@@ -1,10 +1,12 @@
 package kamon.agent.api.instrumentation.interceptor;
 
 import lombok.Builder;
+import lombok.Value;
 import net.bytebuddy.jar.asm.Type;
 import net.bytebuddy.jar.asm.commons.Method;
 
 @Builder
+@Value
 public class Interception {
     private Type interceptorType;
     private Method beforeMethod;
