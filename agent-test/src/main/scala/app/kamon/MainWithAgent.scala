3ffb4c73c765779da039708359eb8d3f1fccfd20
Diego <diegolparra@gmail.com>
2016-02-26 21:36:54 -0300

    wip: some advance over logging

--- agent-test/src/main/scala/app/kamon/MainWithAgent.scala
+++ agent-test/src/main/scala/app/kamon/MainWithAgent.scala
@@ -1,12 +1,6 @@
 package app.kamon
 
-import java.io.{File, FileOutputStream}
-import java.net.URL
-import java.util
-import java.util.Collections
-
 import app.kamon.instrumentation.Pepe
-import kamon.agent.utils.PluginCacheUtils
 import org.slf4j.LoggerFactory
 
 object MainWithAgent {
@@ -21,25 +15,3 @@ object MainWithAgent {
 
 }
 
-object MergeLog4jFuckingDat extends App{
-  import scala.collection.JavaConverters._
-  val tempFiles = new util.ArrayList[File]()
-  val pluginCache = new PluginCacheUtils()
-
-  tempFiles.add(new File("/home/diego/gitHub/kamon-agent/agent/target/scala-2.11/classes/META-INF/org/apache/logging/log4j/core/config/plugins/Log4j2Plugins.dat"))
-  tempFiles.add(new File("/home/diego/Log4j2Plugins.dat"))
-
-//  tempFiles.add(File.createTempFile("Log4j2Plugins2", "dat"))
-
-  pluginCache.loadCacheFiles(getUrls())
-  pluginCache.writeCache(new FileOutputStream(new File("/home/diego/Log4j2PluginsPuto2.dat")))
-
-  def  getUrls(): util.Enumeration[URL] ={
-    val urls = new util.ArrayList[URL]()
-    for (tempFile:File <- tempFiles.asScala) {
-      val url = tempFile.toURI.toURL
-      urls.add(url)
-    }
-    Collections.enumeration(urls)
-  }
-}
