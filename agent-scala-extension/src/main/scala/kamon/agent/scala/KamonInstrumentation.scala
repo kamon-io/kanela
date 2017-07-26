4bd4749c4af94fcdb3610e14fc872e2f8b3554d2
Diego <diegolparra@gmail.com>
2017-07-26 14:27:41 -0300

    introduce method matcher

--- agent-scala-extension/src/main/scala/kamon/agent/scala/KamonInstrumentation.scala
+++ agent-scala-extension/src/main/scala/kamon/agent/scala/KamonInstrumentation.scala
@@ -86,8 +86,11 @@ trait KamonInstrumentation extends JKamonInstrumentation with MethodDescriptionS
 trait MethodDescriptionSugar {
   def isConstructor(): Junction[MethodDescription] = BBMatchers.isConstructor()
   def isAbstract(): Junction[MethodDescription] = BBMatchers.isAbstract()
-  def named(name: String): Junction[MethodDescription] = BBMatchers.named(name)
+  def method(name: String): Junction[MethodDescription] = BBMatchers.named(name)
   def takesArguments(quantity: Int): Junction[MethodDescription] = BBMatchers.takesArguments(quantity)
   def takesArguments(classes: Class[_]*): Junction[MethodDescription] = BBMatchers.takesArguments(classes: _*)
   def withArgument(index: Int, `type`: Class[_]): Junction[MethodDescription] = BBMatchers.takesArgument(index, `type`)
+
+  @deprecated("Use method", "0.0.4" )
+  def named(name: String): Junction[MethodDescription] = method(name)
 }
