d41c31296bc0ce96f039d24383659e25d82f3567
cspinetta <cebs91@gmail.com>
2018-01-04 17:40:56 -0300

    wip

---
+++ travis-test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Licensed under the Apache License, Version 2.0
+# Adapted from https://github.com/paulp/psp-std/blob/master/bin/test
+
+#    && gradle :agent-test:stoppable-instrumentation-spec \
+runTests () {
+  gradle :agent:test \
+    && gradle :agent-test:test \
+    && gradle :agent-test:multi-mixins-spec \
+    && gradle :agent-test:simple-instrumentation-spec \
+    && gradle :agent-test:attach-in-runtime-spec \
+    || exit 1
+  echo "[info] $(date) - finished gradle test"
+}
+
+stripTerminalEscapeCodes () {
+  sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGKM]//g"
+}
+
+mkRegex () { ( IFS="|" && echo "$*" ); }
+
+filterOutput() {
+  while read line; do
+    if ! [[ $(echo $line | stripTerminalEscapeCodes) =~ $excludeRegex ]] ; then
+      echo $line
+    fi
+  done
+}
+
+main() {
+  # gradle output filter
+  local excludeRegex=$(mkRegex \
+    '\[info\] (Resolving|Loading|Updating|Packaging|Done updating|downloading| \[SUCCESSFUL \])' \
+    're[-]run with [-]unchecked for details' \
+    'one warning found'
+  )
+
+  echo "[info] $(date) - starting gradle test"
+  (set -o pipefail && runTests |& filterOutput)
+}
+
+main $@
