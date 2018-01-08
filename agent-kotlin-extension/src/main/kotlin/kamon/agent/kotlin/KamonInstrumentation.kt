2441444452293c44006eecd6e3a1a55f3bc53734
cspinetta <cebs91@gmail.com>
2018-01-08 20:40:14 -0300

    wip

--- agent-kotlin-extension/src/main/kotlin/kamon/agent/kotlin/KamonInstrumentation.kt
+++ agent-kotlin-extension/src/main/kotlin/kamon/agent/kotlin/KamonInstrumentation.kt
@@ -1,3 +1,19 @@
+/*
+ * =========================================================================================
+ * Copyright © 2013-2018 the kamon project <http://kamon.io/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ * =========================================================================================
+ */
+
 package kamon.agent.kotlin
 
 import io.vavr.Function1
@@ -5,32 +21,39 @@ import kamon.agent.api.instrumentation.InstrumentationDescription
 import java.util.function.Supplier
 import kamon.agent.api.instrumentation.KamonInstrumentation as JKamonInstrumentation
 
-open class KamonInstrumentation: JKamonInstrumentation() {
+class KamonInstrumentation: JKamonInstrumentation() {
 
-    fun String.supplied(): Supplier<String> = Supplier { this }
-
-    fun <A, B> ((A) -> B).toVavrFunc(): Function1<A, B> {
-        val underlyingFunc = this
-        return object : Function1<A, B> {
-            override fun apply(v: A): B {
-                return underlyingFunc(v)
-            }
-        }
-    }
-
-    fun forSubtypeOf(name: String, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) = {
+    fun forSubtypeOf(name: String, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) {
         super.forSubtypeOf(name.supplied(), instrumentationFun.toVavrFunc())
     }
 
-    fun forSubtypeOf(names: List<String>, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) = {
+    fun forSubtypeOf(names: List<String>, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) {
         names.forEach { forSubtypeOf(it, instrumentationFun) }
     }
 
-    fun forTargetType(name: String, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) = {
+    fun forTargetType(name: String, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) {
         super.forTargetType(name.supplied(), instrumentationFun.toVavrFunc())
     }
 
-    fun forTargetType(names: List<String>, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) = {
+    fun forTargetType(names: List<String>, instrumentationFun: (InstrumentationDescription.Builder) -> InstrumentationDescription) {
         names.forEach { forTargetType(it, instrumentationFun) }
     }
+}
+
+fun String.supplied(): Supplier<String> = Supplier { this }
+fun <T> Class<T>.supplied(): Supplier<Class<*>> = Supplier { this }
+
+fun <A, B> ((A) -> B).toVavrFunc(): Function1<A, B> {
+    val underlyingFunc = this
+    return object : Function1<A, B> {
+        override fun apply(v: A): B {
+            return underlyingFunc(v)
+        }
+    }
+}
+
+fun kamonInstrumentation(init: KamonInstrumentation.() -> Unit): KamonInstrumentation {
+    val kamonInstrumentation = KamonInstrumentation()
+    kamonInstrumentation.init()
+    return kamonInstrumentation
 }
--- agent-kotlin-extension/src/test/kotlin/kamon/agent/kotlin/KamonInstrumentationSpec.kt
+++ agent-kotlin-extension/src/test/kotlin/kamon/agent/kotlin/KamonInstrumentationSpec.kt
@@ -1,20 +1,64 @@
+/*
+ * =========================================================================================
+ * Copyright © 2013-2018 the kamon project <http://kamon.io/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ * =========================================================================================
+ */
+
 package kamon.agent.kotlin
 
-import io.kotlintest.specs.StringSpec
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import kamon.agent.util.conf.AgentConfiguration
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.lang.instrument.Instrumentation
 
-class KamonInstrumentationSpec : StringSpec() {
 
-    init {
+object KamonInstrumentationSpec : Spek({
 
-        "KamonInstrumentation from kotlin-extension should be constructed well" {
-            val ki = object : KamonInstrumentation() {
-                init {
-                    forSubtypeOf("laala") {
-                        it.build()
-                    }
-                }
-            }
+    describe("a KamonInstrumentation form agent-kotlin-extension") {
+
+        val agentConfigMock = mock<AgentConfiguration.ModuleConfiguration> {
+            on { shouldInjectInBootstrap() } doReturn false
         }
 
+        val instrumentationMock = mock<Instrumentation> {}
+
+        on("instrumenting with a single mixin") {
+            val ki = kamonInstrumentation {
+                forSubtypeOf("laala") {
+                    it
+                            .withMixin(ExampleMixin::class.java.supplied())
+                            .build()
+                }
+            }
+
+            it("should return a single transformation") {
+                val transformations = ki.collectTransformations(agentConfigMock, instrumentationMock)
+                assertEquals(transformations.size, 1)
+                val transformation = transformations[0]
+                assertEquals(transformation.mixins.size(), 1)
+                assertEquals(transformation.bridges.size(), 0)
+                assertEquals(transformation.transformations.size(), 0)
+                assertEquals(transformation.isActive, true)
+                assertEquals(transformation.elementMatcher.isDefined, true)
+            }
+        }
     }
-}
+
+})
+
+class ExampleMixin
