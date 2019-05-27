f0ef2c473efb9d5cb351abdc3e9dc859584091e5
Diego <diegolparra@gmail.com>
2019-05-27 13:09:49 -0300

    minor refactor

--- agent/src/main/java/kanela/agent/util/asm/ClassWriterFlags.java
+++ agent/src/main/java/kanela/agent/util/asm/ClassWriterFlags.java
@@ -12,16 +12,13 @@ import net.bytebuddy.jar.asm.ClassWriter;
 public class ClassWriterFlags {
 
     /**
-     * In versions of the JVM  &gt; 1.5 bytecode, classes contain a stack map along with the method code.
-     *
-     * This map describes the layout of the stack at key points (jump targets) during the method's execution.
+     * In versions of the JVM  &gt; 1.5 the classes bytecode contain a stack map along with the method code. This map describes the layout of the stack at key points (jump targets) during the method's execution.
      *
      * In previous versions(JVM &lt; 1.5 bytecode), the JVM would have to compute this information, which is computationally expensive.
      *
      * By requiring this information, the JVM can just verify that the frames work, which is significantly easier than recalculating everything.
      *
      * So if JVM &lt; 1.5 ClassWriter.COMPUTE_MAXS otherwise ClassWriter.COMPUTE_FRAME(computeFrames implies computeMaxs).
-     *
      */
     public static int resolve(TypeDescription typeDescription, ClassLoader classLoader) {
         if (classFileVersionIsGreaterThan(ClassFileVersion.JAVA_V5, typeDescription, classLoader)) return ClassWriter.COMPUTE_FRAMES;
